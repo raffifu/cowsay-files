1178b828571272e01f133cae55efbe86f05bf422
Charlie Mehrer <charlie.mehrer@firemon.com>
2020-01-21 15:37:18 -0600

    Updating make-examples.sh to ignore files starting with _, and automatically output to examples.md

--- make-examples.sh
+++ make-examples.sh
@@ -1,11 +1,41 @@
 #!/bin/bash
+SCRIPT_DIR=$(dirname "$0")
 
-for cowfile in cows/*.cow; do
-  cowname=$(basename $cowfile)
-  firstchar=${cowname:0:1}
+makeExamples() {
+  for cowfile in ${SCRIPT_DIR}/cows/*.cow; do
+    cowname=$(basename $cowfile)
+    firstchar=${cowname:0:1}
 
-  if [ $firstchar != "_" ]; then
-    # skip any cows that start with _, these are test cows
+    if [ $firstchar != "_" ]; then
+      # skip any cows that start with _, these are test cows
+      echo ""
+      echo "## ${cowname}"
+      cowname="${cowname%.*}"
+
+      imgname=""
+
+      #check if image file matching this cow name exists
+      FILE=converter/src_images/${cowname}.png
+      if test -f "${SCRIPT_DIR}/$FILE"; then
+        imgname="$FILE"
+      fi
+      if [ -z "$imgname" ]; then  #if cow doesn't have a png image related, then just print the cow normally
+          echo '```'
+          cowsay -f ${cowfile} "$cowname"
+          echo '```'
+      else # otherwise insert an image tag for the source image
+        echo "<img src=\"$imgname\" height=\"200\" />"
+      fi
+      echo ""
+    fi
+  done
+
+
+  # Show true color cows separately
+  echo ""
+  echo "# True Color cows"
+  for cowfile in ${SCRIPT_DIR}/cows/true-color/*.cow; do
+    cowname=$(basename $cowfile)
     echo ""
     echo "## ${cowname}"
     cowname="${cowname%.*}"
@@ -13,49 +43,24 @@ for cowfile in cows/*.cow; do
     imgname=""
 
     #check if image file matching this cow name exists
-    FILE=converter/src_images/${cowname}.png
-    if test -f "$FILE"; then
+    FILE="converter/src_images/${cowname}-tc.png"
+    if test -f "${SCRIPT_DIR}/$FILE"; then
       imgname="$FILE"
+    else  # if image doesn't exist, check if one without the -tc suffix exists
+      FILE="converter/src_images/${cowname}.png"
+      if test -f "${SCRIPT_DIR}/$FILE"; then
+        imgname="$FILE"
+      fi
     fi
     if [ -z "$imgname" ]; then  #if cow doesn't have a png image related, then just print the cow normally
         echo '```'
-        cowsay -f ${cowfile} "$cowname"
+        echo "COW SOURCE IMAGE NOT FOUND"
         echo '```'
     else # otherwise insert an image tag for the source image
       echo "<img src=\"$imgname\" height=\"200\" />"
     fi
     echo ""
-  fi
-done
+  done
+}
 
-
-# Show true color cows separately
-echo ""
-echo "# True Color cows"
-for cowfile in cows/true-color/*.cow; do
-  cowname=$(basename $cowfile)
-  echo ""
-  echo "## ${cowname}"
-  cowname="${cowname%.*}"
-
-  imgname=""
-
-  #check if image file matching this cow name exists
-  FILE="converter/src_images/${cowname}-tc.png"
-  if test -f "$FILE"; then
-    imgname="$FILE"
-  else  # if image doesn't exist, check if one without the -tc suffix exists
-    FILE="converter/src_images/${cowname}.png"
-    if test -f "$FILE"; then
-      imgname="$FILE"
-    fi
-  fi
-  if [ -z "$imgname" ]; then  #if cow doesn't have a png image related, then just print the cow normally
-      echo '```'
-      echo "COW NOT FOUND"
-      echo '```'
-  else # otherwise insert an image tag for the source image
-    echo "<img src=\"$imgname\" height=\"200\" />"
-  fi
-  echo ""
-done
+makeExamples > "${SCRIPT_DIR}/examples.md"
