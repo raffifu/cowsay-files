b7f31467e8a9c14f9d1ed6d1a67f34b4466f85d7
Charlie Mehrer <charlie.mehrer@firemon.com>
2020-04-01 09:42:20 -0500

    testing

--- converter/generate-examples.sh
+++ converter/generate-examples.sh
@@ -1,15 +1,12 @@
 EXAMPLES=(
-"michaelangelo" "donatello" "peanuts" "rambo" "dave" "lahey"
-"pickle-rick" "rick-and-morty-split" "rick-and-morty-running"
-"fsm-creation" "this-is-fine" "tf2-spy2"
+"rick-and-morty-split" "donatello" "tobias" "peanuts" "rambo" "dave" "lahey"
+"pickle-rick"  "rick-and-morty-running" "bojack"
+"fsm-creation" "tf2-spy-knife"
 "forrest-gump" "beetlejuice" "jayhawk"
-"bojack" "mr-peanutbutter"
-"stormtrooper2" "vader"
-"batman2" "spiderman3"
-"it-crowd" "kermit"
-"roberto" "calculon"
-"chester"
-"garfield-face" "snoopy-doghouse"
+"stormtrooper2" "vader" "moon2"
+"batman2" "robin" "spiderman3"
+"it-crowd" "kermit" "snoopy-woodstock"
+"roberto" "chester" "garfield" "calvin-and-hobbes"
 )
 
 SCRIPT_DIR=$(dirname "$0")
@@ -24,13 +21,15 @@ function printcow() {
   echo '  <div class="cow-tile">'
   echo "    <div class=\"label\">${1}.cow${EXTRATEXT}</div>"
   echo '    <div class="wrapper">'
+  echo "      <a href=\"https://raw.githubusercontent.com/charc0al/cowsay-files/master/cows/${4}${1}.cow\">"
   echo "        <img src=\"src_images/${COWIMG}.png\" class=\"sizer yeah\">"
-  echo '      </div>'
+  echo '      </a>'
+  echo '    </div>'
   echo '  </div>'
 }
 
 function generate() {
-  echo '<body><h2>Cowsay image example files</h2>'
+  echo '<h2>Cowsay image example files</h2>'
   echo '<link rel="stylesheet" type="text/css" href="src/style.css">'
   echo '<div class="image-container" style="background: #000;">'
 
@@ -56,13 +55,13 @@ function generate() {
       if test -f "${SCRIPT_DIR}/../$COW"; then
         FILE="converter/src_images/${cowname}-tc.png"
         if test -f "${SCRIPT_DIR}/../$FILE"; then
-          printcow $cowname "${cowname}-tc" " (true color)"
+          printcow $cowname "${cowname}-tc" " (true color)" "true-color/"
         fi
       fi
     fi
   done
 
-  echo '</div></body>'
+  echo '</div>'
 }
 
 generate > examples.html
